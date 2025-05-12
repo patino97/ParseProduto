4e09fb5aa9a6698dcd02d9a8324b2f1f33e9b61d
Davi Silva <davipatino54@gmail.com>
2025-05-12 08:22:15 -0300

    criando paginacao

--- app/Http/Controllers/API/ProductsController.php
+++ app/Http/Controllers/API/ProductsController.php
@@ -25,7 +25,7 @@ class ProductsController extends Controller
     public function index(): JsonResponse|AnonymousResourceCollection
     {
       try {
-        $products = Products::useFilters()->get();
+        $products = Products::useFilters()->paginate(15);
 
         return ProductsResource::collection($products);
 
